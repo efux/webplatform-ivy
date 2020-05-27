369442fb9c4f8ae2f8f687fb991cee2db7a7d6f6
Fux, Etienne <etienne.fux@zuehlke.com>
2020-05-27 14:21:26 +0200

    Set repo to initial state for examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,14 @@ import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from
   selector: 'app-root',
   template: `<h1>Webplatform FG - Ivy</h1>
 
-  <button (click)="onClicked()">Add lazy loaded component</button>
-
-  <ng-template #vcr></ng-template>
+  <button>Add lazy loaded component</button>
 
   <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
 
-  @ViewChild('vcr', {read: ViewContainerRef}) vcr: ViewContainerRef;
-
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
-  }
-
-  async onClicked() {
-
-    const infoComp = await import( /* webpackChunkName: 'info-cmp' */ './information/information.component');
-    const factory = this.componentFactoryResolver.resolveComponentFactory(infoComp.InformationComponent);
-    const comp = this.vcr.createComponent(factory);
-
+  constructor() {
   }
 
 }
